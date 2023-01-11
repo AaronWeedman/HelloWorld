245c0f2f1b240be5030e9d603944aafc44d957d3
AaronWeedman <122053740+AaronWeedman@users.noreply.github.com>
2023-01-11 13:23:51 -0500

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author aaron
+ *
+ */
+
+public class Application {
+
+	public static void main(String[] args)
+	{
+		
+		  System.out.println("Hello World");
+
+		
+		
+	}
+}
